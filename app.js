42e51e536d4d824bfd0e7408eeae64c1658af355
Bhabananda Das <blendersbd@gmail.com>
2024-11-13 17:00:24 +0530

    uplod

--- app.js
+++ app.js
@@ -195,8 +195,8 @@ app.get("/dla", async (req, res) => {
     },
   ];
 }*/
-
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
+//const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
